b29da8271b423e54c7410ed22e87e0af28e958ff
Tanjila T <tanjilat@Tanjilas-MacBook-Air.local>
2025-04-18 13:47:48 -0400

    creating classes for both files. Specifically in EnigmaFrame, making sure to extend JFrame and adding guideline comments for what needs to be found in the file.

---
+++ EnigmaFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class EnigmaFrame extends JFrame {
+
+}
+
+/*
+You must use the following GUI elements in completing this lab:
+
+- JComboBox : for selecting the rotor numbers
+- JTextField : for selecting the start of the rotors
+- JTextArea : for providing input to and output from Enigma
+- JButton : for selecting encrypt or decrypt
+- JLabel : for including other text references, such as “Inner” or “Middle”
+*/
---
+++ EnigmaGUI.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class EnigmaGUI {
+    
+}
